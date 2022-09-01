0e50bfebc497d3a17d50e03afc7f539c56c8051d
Konstantina Dureva <k_dureva@yahoo.com>
2022-09-01 23:36:18 +0100

    align

--- sketch_10000.js
+++ sketch_10000.js
@@ -11,34 +11,36 @@ let catImages = [];
 let imageClassifier;
 
 function setup() {
-for (let i = 0; i < 50; i++) {
-    flowerImages[i] = loadImage(`data/flower (${i + 1}).png`);
-    catImages[i] = loadImage(`data/cat (${i + 1}).png`);
-}
+    // 50 was 5000
+    for (let i = 0; i < 50; i++) {
+        flowerImages[i] = loadImage(`data/flower (${i + 1}).png`);
+        catImages[i] = loadImage(`data/cat (${i + 1}).png`);
+    }
 
-createCanvas(400, 400);
+    createCanvas(64, 64);
     // image(flowerImages[0], 0, 0, width, height);
 
-const IMAGE_WIDTH = 64;
-const IMAGE_HEIGHT = 64;
-const IMAGE_CHANNELS = 4;
+    const IMAGE_WIDTH = 64;
+    const IMAGE_HEIGHT = 64;
+    const IMAGE_CHANNELS = 4;
 
-let options = {
-    inputs: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-    // outputs: ['label'],
-    task: 'imageClassification',
-    debug: true,
-};
+    let options = {
+        inputs: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+        // outputs: ['label'],
+        task: 'imageClassification',
+        debug: true,
+    };
 
-imageClassifier = ml5.neuralNetwork(options);
+    imageClassifier = ml5.neuralNetwork(options);
 
-for (let i = 0; i < flowerImages.length; i++) {
-    imageClassifier.addData({ image: flowerImages[i] }, { label: 'Flower' });
-    imageClassifier.addData({ image: catImages[i] }, { label: 'Cat' });
-}
+    for (let i = 0; i < flowerImages.length; i++) {
+        imageClassifier.addData({ image: flowerImages[i] }, { label: 'Flower' });
+        imageClassifier.addData({ image: catImages[i] }, { label: 'Cat' });
+    }
 
-imageClassifier.normalizeData();
-imageClassifier.train({ epochs: 50 }, finishedTraining);
+    imageClassifier.normalizeData();
+
+    imageClassifier.train({ epochs: 50 }, finishedTraining);
 }
 
 function finishedTraining() {
